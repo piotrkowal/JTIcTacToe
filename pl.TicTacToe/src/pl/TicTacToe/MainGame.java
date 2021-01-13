a888c0674eee8d7020d6740d953edcfdaee10b27
Piotr Kowal <piotrkowal2014@gmail.com>
2021-01-13 10:45:47 +0100

    Create and show empty game board

--- pl.TicTacToe/src/pl/TicTacToe/MainGame.java
+++ pl.TicTacToe/src/pl/TicTacToe/MainGame.java
@@ -3,7 +3,17 @@ package pl.TicTacToe;
 public class MainGame {
 
 	public static void main(String[] args) {
+		String[][] gameBoard = new String[3][3];
+		for(int j=0;j<3;j++)
+		for(int i=0;i<3;i++) {
+			gameBoard[i][j] = "-";
+		}
 		
+		for(int j=0;j<3;j++) {
+			for(int i=0;i<3;i++) {
+				System.out.print(gameBoard[i][j]+" ");
+			}
+			System.out.println("");
+		}
 	}
-
 }
