3182f97e1303f62a3fe1b8bb3572739857dfd990
Piotr Kowal <piotrkowal2014@gmail.com>
2021-01-13 12:27:32 +0100

    Created game loop, choices of players are received from input

--- pl.TicTacToe/src/pl/TicTacToe/MainGame.java
+++ pl.TicTacToe/src/pl/TicTacToe/MainGame.java
@@ -1,12 +1,15 @@
 package pl.TicTacToe;
+import java.util.Scanner; 
 
 public class MainGame {
 
 	public static void main(String[] args) {
 		String[][] gameBoard = new String[3][3];
+		int counter = 1;
 		for(int j=0;j<3;j++)
 		for(int i=0;i<3;i++) {
-			gameBoard[i][j] = "-";
+			gameBoard[i][j] = String.valueOf(counter);
+			counter++;
 		}
 		
 		for(int j=0;j<3;j++) {
@@ -15,5 +18,17 @@ public class MainGame {
 			}
 			System.out.println("");
 		}
+		
+		boolean endGame = false;
+        Scanner reader = new Scanner(System.in); 
+        int currentPlayer = 0;
+        
+		while(!endGame) {
+			
+			System.out.println("Player's "+(currentPlayer+1)+" turn");
+			int next = reader.nextInt();
+			currentPlayer = currentPlayer==0?1:0;
+		}
+		
 	}
 }
