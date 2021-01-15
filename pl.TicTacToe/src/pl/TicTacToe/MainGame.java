e66a19a23d9da2233343afddd09af0657d62f7db
Piotr Kowal <piotrkowal2014@gmail.com>
2021-01-15 13:10:27 +0100

    Old code moved to TicTacToeBoard Class

--- pl.TicTacToe/src/pl/TicTacToe/MainGame.java
+++ pl.TicTacToe/src/pl/TicTacToe/MainGame.java
@@ -1,94 +1,23 @@
 package pl.TicTacToe;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.stream.Collectors;
-
 public class MainGame {
 
 	public static void main(String[] args) {
-		int gameBoardSize = 3;
-		String[][] gameBoard = new String[gameBoardSize][gameBoardSize];
-		Map<Integer, HashMap<String, Integer>> coordsOfNum = new HashMap<Integer, HashMap<String, Integer>>();
-		List<Integer> availableNums = new ArrayList<Integer>();
-		int counter = 1;
-		for (int j = 0; j < 3; j++) {
-			for (int i = 0; i < 3; i++) {
-				gameBoard[i][j] = String.valueOf(counter);
+		TicTacToeBoard newGameBoard = new TicTacToeBoard();
+		newGameBoard.setUpBoard();
 
-				HashMap<String, Integer> mapToPutIntoCoords = new HashMap<String, Integer>();
-				mapToPutIntoCoords.put("x", i);
-				mapToPutIntoCoords.put("y", j);
-				coordsOfNum.put(counter, mapToPutIntoCoords);
+		while (!newGameBoard.isEndgame()) {
+			newGameBoard.printOutBoard();
 
-				availableNums.add(counter);
-				counter++;
-			}
+			int playerChoice = newGameBoard.getPlayerChoice();
+
+			newGameBoard.filterOutAvailableNums(playerChoice);
+			newGameBoard.updateGameBoard(playerChoice);
+			newGameBoard.checkIfGameEnded();
+			newGameBoard.changePlayer();
 		}
 
-		boolean endGame = false;
-		Scanner reader = new Scanner(System.in);
-
-		Map<Integer, String> playerSymbols = new HashMap<Integer, String>();
-		playerSymbols.put(0, "X");
-		playerSymbols.put(1, "O");
-		int currentPlayer = 0;
-		int playerWon = -1;
-		while (!endGame) {
-			for (int j = 0; j < 3; j++) {
-				for (int i = 0; i < 3; i++) {
-					System.out.print(gameBoard[i][j] + " ");
-				}
-				System.out.println("");
-			}
-			if (availableNums.isEmpty())
-				break;
-			System.out.println("Player's " + (currentPlayer + 1) + " turn");
-			int playerChoice = reader.nextInt();
-
-			if (availableNums.indexOf(playerChoice) == -1) {
-				do {
-					System.out.println("Choose cell that was't already marked!");
-					playerChoice = reader.nextInt();
-				} while (availableNums.indexOf(playerChoice) == -1);
-			}
-
-			int numToFilterOut = playerChoice;
-			availableNums = availableNums.stream().filter(num -> num != numToFilterOut).collect(Collectors.toList());
-
-			int xCoord = coordsOfNum.get(playerChoice).get("x");
-			int yCoord = coordsOfNum.get(playerChoice).get("y");
-			gameBoard[xCoord][yCoord] = playerSymbols.get(currentPlayer);
-			
-			boolean hasMatch = false;
-			
-				for(int j=0;j<gameBoardSize;j++) {
-					hasMatch = hasMatch || (gameBoard[0][j] == gameBoard[1][j] && gameBoard[1][j] == gameBoard[2][j]); 
-				}
-			
-				for(int i=0;i<gameBoardSize;i++) {
-					hasMatch = hasMatch || (gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][1] == gameBoard[i][2]); 
-				}
-				
-				hasMatch = hasMatch || (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1]==gameBoard[2][2]);
-				hasMatch = hasMatch || (gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1]==gameBoard[2][0]);
-			
-			if(hasMatch) {
-				playerWon = currentPlayer+1;
-				endGame = true;
-			}
-			currentPlayer = currentPlayer == 0 ? 1 : 0;
-		}
-
-		if (playerWon == -1) {
-			System.out.println("All cells have been marked; No one wins!");
-		} else {
-			System.out.println("Player " + playerWon + " wins!");
-		}
-
-		reader.close();
+		newGameBoard.printOutBoard();
+		newGameBoard.showTheWinner();
 	}
 }
--- pl.TicTacToe/src/pl/TicTacToe/TicTacToeBoard.java
+++ pl.TicTacToe/src/pl/TicTacToe/TicTacToeBoard.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Scanner;
+import java.util.stream.Collectors;
 
 public class TicTacToeBoard {
 	private int gameBoardSize = 3;
@@ -11,10 +13,14 @@ public class TicTacToeBoard {
 	private Map<Integer, HashMap<String, Integer>> coordsOfNum;
 	private List<Integer> availableNums;
 	private boolean endGame;
-
+	private int currentPlayer;
+	private int whichPlayerWon;
 	Map<Integer, String> playerSymbols;
+	Scanner playerChoiceReader;
 
 	public TicTacToeBoard() {
+		this.currentPlayer = 0;
+		this.whichPlayerWon = -1;
 		this.endGame = false;
 		this.gameBoard = new String[gameBoardSize][gameBoardSize];
 		this.coordsOfNum = new HashMap<Integer, HashMap<String, Integer>>();
@@ -23,6 +29,8 @@ public class TicTacToeBoard {
 		this.playerSymbols = new HashMap<Integer, String>();
 		this.playerSymbols.put(0, "X");
 		this.playerSymbols.put(1, "O");
+
+		this.playerChoiceReader = new Scanner(System.in);
 	}
 
 	public void setUpBoard() {
@@ -42,7 +50,6 @@ public class TicTacToeBoard {
 		}
 	}
 
-
 	public void printOutBoard() {
 		for (int j = 0; j < 3; j++) {
 			for (int i = 0; i < 3; i++) {
@@ -51,5 +58,71 @@ public class TicTacToeBoard {
 			System.out.println("");
 		}
 	}
-	
+
+	public int getPlayerChoice() {
+		System.out.println("Player's " + (this.currentPlayer + 1) + " turn");
+
+		int playerChoice = this.playerChoiceReader.nextInt();
+
+		if (this.availableNums.indexOf(playerChoice) == -1) {
+			do {
+				System.out.println("Choose cell that was't already marked!");
+				playerChoice = this.playerChoiceReader.nextInt();
+			} while (this.availableNums.indexOf(playerChoice) == -1);
+		}
+		return playerChoice;
+	}
+
+	public void filterOutAvailableNums(int playerChoice) {
+		this.availableNums = this.availableNums.stream().filter(num -> num != playerChoice)
+				.collect(Collectors.toList());
+	}
+
+	public void updateGameBoard(int playerChoice) {
+		var cellCoordsToUpdate = coordsOfNum.get(playerChoice);
+		int xCoord = cellCoordsToUpdate.get("x");
+		int yCoord = cellCoordsToUpdate.get("y");
+		gameBoard[xCoord][yCoord] = playerSymbols.get(this.currentPlayer);
+	}
+
+	public void changePlayer() {
+		this.currentPlayer = this.currentPlayer == 0 ? 1 : 0;
+	}
+
+	public boolean isEndgame() {
+		return this.endGame;
+	}
+
+	public void checkIfGameEnded() {
+		boolean hasMatch = false;
+
+		for (int j = 0; j < gameBoardSize; j++) {
+			hasMatch = hasMatch || (gameBoard[0][j] == gameBoard[1][j] && gameBoard[1][j] == gameBoard[2][j]);
+		}
+
+		for (int i = 0; i < gameBoardSize; i++) {
+			hasMatch = hasMatch || (gameBoard[i][0] == gameBoard[i][1] && gameBoard[i][1] == gameBoard[i][2]);
+		}
+
+		hasMatch = hasMatch || (gameBoard[0][0] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][2]);
+		hasMatch = hasMatch || (gameBoard[0][2] == gameBoard[1][1] && gameBoard[1][1] == gameBoard[2][0]);
+
+		if (hasMatch || availableNums.isEmpty()) {
+			this.endGame = true;
+		}
+
+		if (hasMatch && !availableNums.isEmpty()) {
+			this.whichPlayerWon = currentPlayer + 1;
+		}
+
+	}
+
+	public void showTheWinner() {
+		if (this.whichPlayerWon == -1) {
+			System.out.println("All cells have been marked; No one wins!");
+		} else {
+			System.out.println("Player " + this.whichPlayerWon + " wins!");
+		}
+
+	}
 }
